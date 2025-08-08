7b4ec99797b84c79db33df653992678b3f9f4c97
tonyg25 <tony@vivapr.co.uk>
2025-08-08 12:34:16 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("Hello from Streamlit")
-st.write("If you can see this, your deploy works. Next, switch back to app.py.")
+st.write("✅ If you see this, the hosting works fine.")
